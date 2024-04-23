9584d503e215dc334e471757408b8a4f7f2bcbdd
TOFIKS808 <113688857+TOFIKS808@users.noreply.github.com>
2024-04-23 12:06:45 +0200

    test for API handling for single company (#4)

---
+++ tests/api/test_company.py
@@ -0,0 +1,38 @@
+# pylint: disable=R0801
+"""
+    Tests for functional tests for user endpoints.
+"""
+import unittest
+from fastapi.testclient import TestClient
+from tests.DbTestCase import DbTestTestCase
+
+from main import app
+
+
+class UsersTestCase(DbTestTestCase):
+    """
+        Tests for functional tests for user endpoints.
+    """
+
+    def setUp(self):
+        """ setup test environment. """
+        self.client = TestClient(app)
+        super().setUp()
+
+    def test_address_get_item_not_existing(self):
+        """ Test GET request to /address/{id} with invalid id. """
+        response = self.client.get("/address/-1")
+        self.assertEqual(404, response.status_code)
+
+    def test_company_get_item(self):
+        """ Test GET request to /address/{id} with valid id. """
+        response = self.client.get("/address/1")
+        self.assertEqual(200, response.status_code)
+
+        address = response.json()
+        self.assertEqual(1, address.get('id'))
+        self.assertEqual(1, address.get('geo').get('id'))
+
+
+if __name__ == '__main__':
+    unittest.main()
