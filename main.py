0243c5b4c615dee220591c73b2e80c31ac1f8393
TOFIKS808 <113688857+TOFIKS808@users.noreply.github.com>
2024-04-23 12:47:31 +0200

    API handling for single geo unit (#6)

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from src.api.users import users_get_item
 from src.api.company import company_get_item
 from src.api.address import address_get_item
+from src.api.geo import geo_get_item
 
 app = FastAPI()
 
@@ -15,13 +16,18 @@ def api_users_get_item(id: int):
     """ get user endpoint"""
     return users_get_item(id)
 
-@app.get("/company/{id}", tags=["Company"])
+@app.get("/companies/{id}", tags=["Company"])
 def api_users_get_item(id: int):
     """ get company endpoint"""
     return company_get_item(id)
 
-@app.get("/address/{id}", tags=["Address"])
+@app.get("/addresses/{id}", tags=["Address"])
 def api_users_get_item(id: int):
     """ get address endpoint"""
     return address_get_item(id)
+
+@app.get("/geo/{id}", tags=["Geo"])
+def api_users_get_item(id: int):
+    """ get geo endpoint"""
+    return geo_get_item(id)
 
